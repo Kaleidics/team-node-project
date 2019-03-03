af92d37ce1d546760cec973c49851e45f2f00bf3
Eddie Chu <contact@eddiechu.me>
2019-03-03 00:17:52 -0800

    debug nav againnnn

--- public/script.js
+++ public/script.js
@@ -737,11 +737,12 @@ function mobileNav() {
         // $('#signUpBtn').toggleClass('overlaySpacing');
     });
     $('#loginBtn, #signUpBtn').on('click', (event) => {
-        $('.content').toggleClass('change');
-        $('.mDelta').toggle('mobileHide');
-        
-        $('.nav').removeClass('heightMobile');
-        $('.navbar li').toggleClass('overlaySpacing');
+        if ($('.content').hasClass('change')) {
+            $('.content').toggleClass('change');
+            $('.mDelta').toggle('mobileHide');
+            $('.nav').removeClass('heightMobile');
+            $('.navbar li').toggleClass('overlaySpacing');
+        }
     })
     if ($(window).width() > 1024) {
         // $('.content').toggleClass('change');
